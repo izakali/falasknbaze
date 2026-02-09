c035b06bf4149813f3fa1748da2b08c8a7c63311
izakali <izakaicha@gmail.com>
2026-02-09 19:25:39 +0100

    update

--- scripts/views/home.js
+++
@@ -1,9 +0,0 @@
-export default () => {
-    return `
-        <section class="hero">
-            <h2>Bienvenue chez FALASK</h2>
-            <p>Votre solution de distribution d'eau gratuite via notre application mobile.</p>
-            <a href="#" class="cta-button">Téléchargez l'application</a>
-        </section>
-    `;
-};
